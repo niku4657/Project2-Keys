dda4e67eb8d226defa8843486190f5c7f5456f8e
437kathleen <49416686+437kathleen@users.noreply.github.com>
2020-03-02 22:05:59 -0700

    Update add_user.py

--- Server/add_user.py
+++ Server/add_user.py
@@ -1,20 +1,31 @@
 """
+
     add_user.py - Stores a new username along with salt/password
 
+
+
     CSCI 3403
+
     Authors: Matt Niemiec and Abigail Fernandes
+
     The solution contains the same number of lines (plus imports)
+
 """
-import hashlib 
+
+from Crypto import Random
+from Crypto.Hash import SHA256
+import sys
 import os
 
 user = input("Enter a username: ")
 password = input("Enter a password: ")
 
 # TODO: Create a salt and hash the password
-salt = str(os.urandom(16))
-hashed_password = hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
+salt = str(Random.get_random_bytes(32))
+password_and_salt = password+salt
+hashed_password = SHA256.new(str.encode(password_and_salt)).hexdigest()
 
+# print(os.path.join(sys.path[0], "passfile.txt"))
 
 try:
     reading = open("passfile.txt", 'r')
@@ -25,7 +36,6 @@ try:
     reading.close()
 except FileNotFoundError:
     pass
-
 with open("passfile.txt", 'a+') as writer:
     writer.write("{0}\t{1}\t{2}\n".format(user, salt, hashed_password))
     print("User successfully added!")
