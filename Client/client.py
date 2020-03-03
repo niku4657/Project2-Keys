df88a827d4faaea749f0bfe23463d56604338a9e
NehaKuna <32585365+NehaKuna@users.noreply.github.com>
2020-03-02 18:20:45 -0700

    ed

--- Client/client.py
+++ Client/client.py
@@ -48,8 +48,8 @@ def generate_key():
 def encrypt_handshake(session_key):
     # TODO: Implement this function
     public = RSA.importKey(open('ppkey.txt.pub','r').read())
-    encrypt = str(public.encrypt(session_key, 32))
-    return encrypt
+    encrypted = str(public.encrypt(session_key, 16))
+    return encrypted
 
 
 # Encrypts the message using AES. Same as server function
@@ -81,51 +81,6 @@ def send_message(sock, message):
 def receive_message(sock):
     data = sock.recv(1024)
     return data
-
-
-# def main():
-#     generate_key()
-#     user = input("What's your username? ")
-#     password = input("What's your password? ")
-#
-#     # Create a TCP/IP socket
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-#     # Connect the socket to the port where the server is listening
-#     server_address = (host, port)
-#     print('connecting to {} port {}'.format(*server_address))
-#     sock.connect(server_address)
-#
-#     try:
-#         # Message that we need to send
-#         message = user + ' ' + password
-#
-#         # Generate random AES key
-#         key = generate_key()
-#
-#         # Encrypt the session key using server's public key
-#         encrypted_key = encrypt_handshake(key)
-#
-#         # Initiate handshake
-#         send_message(sock, encrypted_key)
-#
-#         # Listen for okay from server (why is this necessary?)
-#         if receive_message(sock).decode() != "okay":
-#             print("Couldn't connect to server")
-#             exit(0)
-#
-#         # TODO: Encrypt message and send to server
-#         encryptedMessage = encrypt_message(message, key)
-#         send_message(sock, encryptedMessage)
-#
-#         # TODO: Receive and decrypt response from server
-#         if(receive_message(sock)):
-#             print("client received_message", decrypt_message(receive_message(sock), key))
-#
-#     finally:
-#         print('closing socket')
-#         sock.close()
-
 
 def main():
     generate_key()
